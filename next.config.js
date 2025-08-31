b71e0e99252e59d7259edcfb8b00044d7fc2264d
ISORegret <rtaylorbrick@gmail.com>
2025-08-31 17:40:50 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' }
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'i.imgur.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      // Pixieset cover images (if you hotlink one)
+      { protocol: 'https', hostname: '*.pixieset.com' }
     ]
   }
 };
