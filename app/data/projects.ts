e68e84fdb0640a213a3a5e4d210736555fb84560
ISORegret <rtaylorbrick@gmail.com>
2025-08-31 23:24:21 -0400

    Update projects.ts

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -17,17 +17,17 @@ export const projects: Project[] = [
     date: '2025-08-23',
     cover: 'https://images.pixieset.com/32995689/d861118a57dc9048fc001b78e24f7b52-large.jpg',
     album: 'https://isoregret.pixieset.com/morningmotorevent/',
-    blurb: 'Sunrise rollers and chrome for days.'
+    blurb: 'Sunrise shots and chrome for days.'
   },
   
   {
   slug: 'Opening of the Beaches',
-  title: 'Ooening of the Beaches',
+  title: 'Opening of the Beaches',
   category: 'Automotive',  // must match one of: 'Automotive' | 'Real Estate' | 'Street'
   date: '2025-09-01',  // use YYYY-MM-DD
   cover: 'https://images.pixieset.com/09906689/eb66a76e5bd0a204ea4f344b0b1cafbe-large.jpg',  // thumbnail image
   album: 'https://isoregret.pixieset.com/OpeningoftheBeaches/',  // link to full album
-  blurb: 'Golden hour portraits and skyline views in downtown Jacksonville.',
+  blurb: 'Golden hour portraits and skyline views in downtown Jacksonville Beach.',
   },
   
   {
